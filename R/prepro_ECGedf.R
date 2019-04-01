c52ff77b66b241fd7ee177d23d8c84cae287d165
zenBen <ben.cowley@helsinki.fi>
2019-04-01 18:15:58 +0300

    Much DYNECOM synchrony analysis.

--- R/prepro_ECGedf.R
+++ R/prepro_ECGedf.R
@@ -1,20 +1,47 @@
 library(lubridate)
 library(colibri)
-source('read_parse_data.R')
+library(tidyverse)
+# source('read_parse_data.R')
 
 basepath <- "/home/bcowley/Benslab/EMOSYNC/DYNECOM/ECGedf/"
 setwd(basepath)
 
-file_list <- list.files(pattern = ".*edf") #, full.names = TRUE)
+file_list <- list.files(pattern = ".*6min.edf") #, full.names = TRUE)
+PLOT_RAW <- TRUE
+WNDW <- 60
+
+# logf <- "log_file.txt"
+plotdir <- "ECGplots/"
+if (!dir.exists(plotdir))
+   dir.create(plotdir, FALSE)
+csvdir <- "HRVcsv/"
+if (!dir.exists(csvdir))
+  dir.create(csvdir, FALSE)
+
+# DEBUG
+# fname <- 'DCVRa_D_both_0001-ECG-6min.edf'
+# results <- vector("list", length(file_list))
+START <- 1
+file_list <- file_list[START:length(file_list)]
+i = START - 1
 
 for (fname in file_list)
 {
+  i = i+1
+  fname_sans_ext <- tools::file_path_sans_ext(fname)
+  part_num <- as.numeric(gsub("[^0-9]", "", substr(fname, 1, 18)))
   ## read the data
   recording <- read.data.edf(fname)
   recording$signal$ECG$data <- -recording$signal$ECG$data  ## need to flip the polarity
   
   ## detect the R-peaks
-  recording <- ibi_from_ecg(recording)
+  recording <- tryCatch({
+    ibi_from_ecg(recording) 
+  }, error = function(e){ 
+    print(paste(i, ":: FAIL ::", fname_sans_ext, ":: MYSTERIOUS ERROR IN ibi_from_ecg()!!"))
+    return(e)
+  })
+  if(inherits(recording, "error")) next
   
   ## remove artifacts
   ibi_orig <- recording$signal$ibi
@@ -25,104 +52,61 @@ for (fname in file_list)
     recording$signal$ibi$data <- recording$signal$ibi$data[-ind.artifact]
     recording$signal$ibi$t <- recording$signal$ibi$t[-ind.artifact]
   }
+  ## plot ecg with R-peaks
+  if (PLOT_RAW)
+  {
+    png(file = paste0(plotdir, fname_sans_ext, ".png"), width = 1000, height = 1000)
+    par(mfrow = c(3, 1))
+    plot(recording$signal$ECG$t, recording$signal$ECG$data, type = "l", col = "blue", lwd = 1)
+    points(recording$signal$ibi.amp$t,recording$signal$ibi.amp$data, type = "p", col = "red", cex = 2, pch = 20)
+    plot(ibi_orig$t, ibi_orig$data, type = "l", col = "red", lwd = 1)
+    plot(recording$signal$ibi$t, recording$signal$ibi$data, type = "l", col = "red", lwd = 1)
+    dev.off()
+  }
+  ## calculate whether denoised data can be used to estimate results
+  # number of sliding windows X with Y overlap in N-element vector is given by N-Y / X-Y
+  # in 6 mins (360 secs) you get 11 windows of 60 secs with 50% overlap
+  END_IBI <- tail(recording$signal$ibi$t, 1)
+  if (END_IBI < 260) # this data cannot provide 11 windows of data with overlap <= 40
+  {
+    print(paste(i, ":: FAIL ::", fname_sans_ext, ":: AFTER DE-NOISING, DATA IS TOO SHORT TO ANALYZE!!"))
+    next
+  }else{
+    OVERLAP <- round((660 - END_IBI) / 10) + 1
+  }
+  ## Add a block
+  block_1 <- create_block_simple(starttime = format(recording$properties$time.start, format = "%Y%m%dT%H%M%S"),
+                                 starttype = "timestamp",
+                                 stoptime  = format(recording$properties$time.start + round(END_IBI) - 1, format = "%Y%m%dT%H%M%S"),
+                                 stoptype  = "timestamp",
+                                 tasktype  = substr(fname, 1, 12),
+                                 part = part_num,
+                                 meas = 3,
+                                 blockid = 1,
+                                 subject = NA,
+                                 casename = fname_sans_ext,
+                                 recording = NULL)
+  recording <- create_block_structure(recording)
+  ## set the zerotime
+  recording <- recording_set_zerotime(recording, timestamp = recording$properties$zerotime)
+  recording <- add_block(recording, block_1)
+  
+  ## analyze the recording
+  ## Settings
+  settings                 <- settings_template_hrv()
+  settings$segment.length  <- WNDW
+  settings$segment.overlap <- OVERLAP
+  recording$conf$settings  <- settings
+  
+  ## Analyze the recording
+  recording <- analyze_recording(recording, settings, signal = "ibi", analysis.pipeline = analysis_pipeline_ibi)
+  results <- collect_results(recording)
+  print(paste(i, ":: SUCCESS ::", fname_sans_ext, "::", tail(results$segmentid, 1), "windows of length", WNDW, "and overlap", OVERLAP))
+  
+  results <- filter(results, variable %in% c("stdevhr", "rmssd")) %>% 
+    select(value, segment, variable, part, meas, tasktype) %>% 
+    spread(variable, value)
+  write.csv(results, paste0('HRVcsv/', fname_sans_ext, '.csv'))
   
 }
 
-
-## plot ecg with R-peaks
-x11()
-par(mfrow = c(3, 1))
-
-plot(recording$signal$ECG$t[1:3000],
-     recording$signal$ECG$data[1:3000], type = "l", col = "blue", lwd = 1)
-points(recording$signal$ibi.amp$t[1:31],
-       recording$signal$ibi.amp$data[1:31], type = "p", col = "red", cex = 2,
-       pch = 20)
-
-plot(ibi_orig$t, ibi_orig$data, type = "l", col = "red", lwd = 1)
-
-plot(recording$signal$ibi$t, recording$signal$ibi$data, type = "l",
-     col = "red", lwd = 1)
-
-## Add a block
-##
-## $ time.start    : POSIXct[1:1], format: "1985-01-01 21:58:17"
-## $ time.stop     : POSIXct[1:1], format: "1985-01-02 09:20:17"
-
-block_1 <- create_block_simple(starttime = "19850101T223000",
-                               starttype = "timestamp",
-                               stoptime  = "19850101T233000",
-                               stoptype  = "timestamp",
-                               tasktype  = "sleep (start of night)",
-                               part = 1,
-                               meas = 1,
-                               blockid = 1,
-                               subject = NA,
-                               casename = "demo01",
-                               recording = NULL)
-
-block_2 <- create_block_simple(starttime = "19850102T045000",
-                               starttype = "timestamp",
-                               stoptime  = "19850102T052500",
-                               stoptype  = "timestamp",
-                               tasktype  = "sleep (end of night)",
-                               part = 1,
-                               meas = 2,
-                               blockid = 1,
-                               subject = NA,
-                               casename = "demo01",
-                               recording = NULL)
-
-recording <- create_block_structure(recording)
-
-## Visalise the blocks
-b1s <- block_to_seconds(recording, block_1)
-b2s <- block_to_seconds(recording, block_2)
-
-x11()
-plot(recording$signal$ibi$t, recording$signal$ibi$data, type = "l")
-abline(v = c(b1s$starttime, b1s$stoptime), col = "red", lwd = 5)
-abline(v = c(b2s$starttime, b2s$stoptime), col = "red", lwd = 5)
-
-## set start time to UTC
-recording$properties$time.start <-
-  force_tz(recording$properties$time.start, "UTC")
-recording$properties$time.stop <-
-  force_tz(recording$properties$time.stop, "UTC")
-
-## set the zerotime
-recording <- recording_set_zerotime(recording, timestamp =
-                                      str_to_timestamp("19850101T215817 EEST"))
-recording <- add_block(recording, block_1)
-recording <- add_block(recording, block_2)
-
-
-## analyze the recording
-## Settings
-settings                 <- settings_template_hrv()
-settings$segment.length  <- 30
-settings$segment.overlap <- 0
-recording$conf$settings  <- settings
-
-## Analyze the recording
-cat("\t analyzing ibi data\n")
-recording <- analyze_recording(recording, settings, signal = "ibi",
-                               analysis.pipeline = analysis_pipeline_ibi)
-results <- collect_results(recording)
-#graphics.off()
-
-## Visualise results
-x11()
-par(mfrow = c(2, 1))
-plot_metric(recording, metric = "mean", filename = NULL, blockid = 1,
-            new.plot = FALSE)
-plot_metric(recording, metric = "mean", filename = NULL, blockid = 2,
-            new.plot = FALSE)
-
-x11()
-library(ggplot2)
-res_mean <- subset(results, variable == "meanhr")
-p <- ggplot(res_mean, aes(value, fill = tasktype))
-p <- p + geom_density()
-p <- p + theme_bw()
-print(p)
