1926900f964fe1b41c9627588632352b854ced24
zenben <ben.cowley@helsinki.fi>
2020-11-10 13:39:12 +0200

    Ch ch ch changes

--- PPAC/R/cossim_sync_method1.R
+++ PPAC/R/cossim_sync_method1.R
@@ -10,22 +10,23 @@ library(here)
 source(file.path(here(), 'R', 'cossim_sync_method_functions.R'))
 
 polku <- file.path(here(), 'data', 'ppac_all.csv')
-tiedosto <- read.csv(polku, header = T, sep = ",")
+PPAC <- read.csv(polku, header = T, sep = ",")
 
-sync_2feats_1emo(tiedosto, "anger", orb_avg, fasym_rel, TIMECOR = F)
+sync_2feats_1emo(PPAC, "anger", orb_avg, fasym_rel, TIMECOR = F)
 
 ###
-fnames <- colnames(select(tiedosto, 5:17))
+# fnames <- subset somehow
+fnames <- colnames(select(PPAC, c(5, 9, 10, 11, 13)))
 flen <- length(fnames)
-emotions <- unique(tiedosto$event)
+emotions <- unique(PPAC$event)
 emosync <- NULL
 
 for (e in emotions){
-  allsync <- sync_2feats_1emo(tiedosto, e, fnames[1], fnames[2], FALSE)
+  allsync <- sync_2feats_1emo(PPAC, e, fnames[1], fnames[2], FALSE)
   
   for (i in 2:flen - 1) {
     for (j in (i+1) : flen) {
-      tmp <- sync_2feats_1emo(tiedosto, e, fnames[i], fnames[j], FALSE)
+      tmp <- sync_2feats_1emo(PPAC, e, fnames[i], fnames[j], FALSE)
       allsync <- merge(allsync, tmp, sort = F)
     }
   }
@@ -37,3 +38,34 @@ for (e in emotions){
 emosyncL <- emosync %>%
   pivot_longer(cols = starts_with("cos.sim_"), names_to = "feat.pair", values_to = "cos.sim") %>%
   mutate(feat.pair = gsub("cos.sim_", "", feat.pair))
+
+# emosyncLsort <- emosyncL %>%
+#   group_by(ID) %>%
+#   mutate(idx = 1:n()) %>%
+#   arrange(cos.sim, .by_group = TRUE)
+
+testi <- emosyncL %>% 
+  group_by(ID, emotion) %>% 
+  summarise(sync = mean(abs(na.omit(cos.sim))))
+
+test2 <- emosyncL %>% 
+  group_by(emotion) %>% 
+  summarise(sync = mean(na.omit(cos.sim)))
+
+test3 <- emosyncL %>% 
+  group_by(feat.pair) %>% 
+  summarise(sync = mean(na.omit(cos.sim)))
+
+test4 <- emosyncL %>% 
+  filter(feat.pair == "zyg_avg_orb_avg") %>%
+  group_by(emotion) %>%
+  summarise(zyg.orb.sync = mean(na.omit(cos.sim)))
+
+test5 <- emosyncL %>% 
+  filter(feat.pair == "zyg_avg_orb_avg") %>%
+  group_by(ID) %>%
+  summarise(zyg.orb.sync = mean(na.omit(cos.sim)))
+
+df <- merge(test5, perf)
+
+summary(lm(visAcc ~ zyg.orb.sync, data = df))
