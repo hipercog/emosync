01205065c767825d279e0fefd684ae69f715cb69
zenben <ben.cowley@helsinki.fi>
2020-11-10 16:02:31 +0200

    Ch ch ch changes

--- PPAC/R/cossim_sync_method1.R
+++ PPAC/R/cossim_sync_method1.R
@@ -16,7 +16,8 @@ sync_2feats_1emo(PPAC, "anger", orb_avg, fasym_rel, TIMECOR = F)
 
 ###
 # fnames <- subset somehow
-fnames <- colnames(select(PPAC, c(5, 9, 10, 11, 13)))
+featnames <- colnames(select(PPAC, 5:17))
+fnames <- featnames[c(1, 5, 6, 7, 8, 9)]
 flen <- length(fnames)
 emotions <- unique(PPAC$event)
 emosync <- NULL
@@ -39,32 +40,38 @@ emosyncL <- emosync %>%
   pivot_longer(cols = starts_with("cos.sim_"), names_to = "feat.pair", values_to = "cos.sim") %>%
   mutate(feat.pair = gsub("cos.sim_", "", feat.pair))
 
-# emosyncLsort <- emosyncL %>%
-#   group_by(ID) %>%
-#   mutate(idx = 1:n()) %>%
-#   arrange(cos.sim, .by_group = TRUE)
-
+# average ID x emotion across all feat pairs
 mean.cos.sim <- emosyncL %>% 
   group_by(ID, emotion) %>% 
   summarise(sync = mean(abs(na.omit(cos.sim))))
 
+# average each emotion across all feat pairs
 mean.emo <- emosyncL %>% 
   group_by(emotion) %>% 
   summarise(sync = mean(na.omit(cos.sim)))
 
+# average each feat pair across all ID x emotion
 mean.feat.pair <- emosyncL %>% 
   group_by(feat.pair) %>% 
   summarise(sync = mean(na.omit(cos.sim)))
 
+# filter to Zyg x Orb and average across all ID
 mean.emo.zyg.orb <- emosyncL %>% 
   filter(feat.pair == "zyg_avg_orb_avg") %>%
   group_by(emotion) %>%
   summarise(zyg.orb.sync = mean(na.omit(cos.sim)))
 
+# filter to Zyg x Orb and average across all emotions
 mean.id.zyg.orb <- emosyncL %>% 
   filter(feat.pair == "zyg_avg_orb_avg") %>%
   group_by(ID) %>%
   summarise(zyg.orb.sync = mean(na.omit(cos.sim)))
+
+# filter to Anger and average across all ID x emotion
+mean.feat.pair.anger <- emosyncL %>% 
+  filter(emotion == "anger") %>%
+  group_by(feat.pair) %>%
+  summarise(sync = mean(na.omit(cos.sim)))
 
 df <- merge(test5, perf)
 
